9855e09b97e60ed0ce7407d1229de71acd903c87
Pranav Singh <pranavvalt@gmail.com>
2024-01-24 21:16:16 +0530

    navbar setup

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
    const [headers, setHeaders] = useState<string[]>([])
    const [parsedData, setParsedData] = useState<FileType[]>([])
    return (
-      <div className="w-full p-2 mt-5 md:p-5 md:mt-0">
+      <div className="w-full md:w-[calc(100%-300px)] p-2 mt-5 md:p-5 md:mt-0">
          <h1 className="text-xl font-bold">Upload CSV</h1>
 
          <Upload
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,8 +26,9 @@ const Dashboard = () => {
    const [open, setOpen] = useState(false)
    return (
       <>
-         <div className="relative w-full hidden md:flex flex-row">
-            <div className="w-72 h-screen p-5 ">
+         <div className="relative flex flex-col-reverse md:flex-row">
+            {/* above medium nav bar */}
+            <div className="w-[300px] h-screen p-5 border-e hidden md:block">
                <div className="flex justify-center items-center gap-2 mb-10">
                   <img src={logo} alt="" className="h-[40px] rounded-full" />
                   <p className="text-2xl">Base</p>
@@ -45,20 +46,37 @@ const Dashboard = () => {
                      </div>
                   ))}
                </div>
+               {/* above medium user */}
+               <div className="absolute flex gap-5 top-5 right-5 ">
+                  <FaRegBell className="text-2xl" />
+                  <img
+                     src={person}
+                     alt=""
+                     className="h-[25px] p-[1px] rounded-full border-2 border-black"
+                  />
+               </div>
             </div>
             <Main />
-            <div className="absolute flex gap-5 top-5 right-5 ">
-               <FaRegBell className="text-2xl" />
-               <img
-                  src={person}
-                  alt=""
-                  className="h-[25px] p-[1px] rounded-full border-2 border-black"
-               />
+            <div className="sticky md:hidden top-0 left-0 h-[60px] flex justify-between items-center p-3 w-full bg-neutral-50">
+               <div className="flex justify-center items-center gap-1">
+                  <FiMenu
+                     className="text-2xl mr-2"
+                     onClick={() => setOpen(true)}
+                  />
+                  <img src={logo} alt="" className="h-[35px] rounded-full" />
+                  <p className="text-2xl">Base</p>
+               </div>
+               <div className="flex gap-5">
+                  <FaRegBell className="text-2xl" />
+                  <img
+                     src={person}
+                     alt=""
+                     className="h-[25px] p-[1px] rounded-full border-2 border-black"
+                  />
+               </div>
             </div>
-         </div>
-         <div className="relative w-full md:hidden">
             {open && (
-               <div className="absolute md:hidden bg-neutral-50 h-screen w-[300px] z-30 border-e py-3 px-5">
+               <div className="fixed top-0 bg-neutral-50 md:hidden h-full w-[300px] z-30 border-e py-3 px-5">
                   <div className="flex justify-between items-center mb-10">
                      <div className="flex justify-between items-center gap-1">
                         <img
@@ -88,28 +106,93 @@ const Dashboard = () => {
                   </div>
                </div>
             )}
-
-            <div className="sticky top-0 left-0 h-[60px] flex justify-between items-center p-3 w-full">
-               <div className="flex justify-center items-center gap-1">
-                  <FiMenu
-                     className="text-2xl mr-2"
-                     onClick={() => setOpen(true)}
-                  />
-                  <img src={logo} alt="" className="h-[35px] rounded-full" />
-                  <p className="text-2xl">Base</p>
-               </div>
-               <div className="flex gap-5">
-                  <FaRegBell className="text-2xl" />
-                  <img
-                     src={person}
-                     alt=""
-                     className="h-[25px] p-[1px] rounded-full border-2 border-black"
-                  />
-               </div>
-            </div>
-            <Main />
          </div>
       </>
+      // <>
+      //    <div className="relative w-full hidden md:flex flex-row">
+      //       <div className="w-72 h-screen p-5 ">
+      //          <div className="flex justify-center items-center gap-2 mb-10">
+      //             <img src={logo} alt="" className="h-[40px] rounded-full" />
+      //             <p className="text-2xl">Base</p>
+      //          </div>
+      //          <div className="flex flex-col gap-7 items-center">
+      //             {links.map((link) => (
+      //                <div
+      //                   key={link.label}
+      //                   className={`flex justify-start items-center gap-2 w-28 hover:text-indigo-500 ${
+      //                      link.label === "Upload" ? "text-indigo-600" : ""
+      //                   }`}
+      //                >
+      //                   <span className="text-2xl">{link.icon}</span>
+      //                   <p className="">{link.label}</p>
+      //                </div>
+      //             ))}
+      //          </div>
+      //       </div>
+      //       <Main />
+      // <div className="absolute flex gap-5 top-5 right-5 ">
+      //    <FaRegBell className="text-2xl" />
+      //    <img
+      //       src={person}
+      //       alt=""
+      //       className="h-[25px] p-[1px] rounded-full border-2 border-black"
+      //    />
+      // </div>
+      //    </div>
+      //    <div className="relative w-full md:hidden">
+      // {open && (
+      //    <div className="absolute md:hidden bg-neutral-50 h-screen w-[300px] z-30 border-e py-3 px-5">
+      //       <div className="flex justify-between items-center mb-10">
+      //          <div className="flex justify-between items-center gap-1">
+      //             <img
+      //                src={logo}
+      //                alt=""
+      //                className="h-[35px] rounded-full"
+      //             />
+      //             <p className="text-2xl">Base</p>
+      //          </div>
+      //          <RxCross2
+      //             className="text-2xl mr-2"
+      //             onClick={() => setOpen(false)}
+      //          />
+      //       </div>
+      //       <div className="flex flex-col gap-7 items-start ps-5">
+      //          {links.map((link) => (
+      //             <div
+      //                key={link.label}
+      //                className={`flex justify-start items-center gap-2 w-28 hover:text-indigo-500 ${
+      //                   link.label === "Upload" ? "text-indigo-600" : ""
+      //                }`}
+      //             >
+      //                <span className="text-2xl">{link.icon}</span>
+      //                <p className="">{link.label}</p>
+      //             </div>
+      //          ))}
+      //       </div>
+      //    </div>
+      // )}
+
+      // <div className="sticky top-0 left-0 h-[60px] flex justify-between items-center p-3 w-full">
+      //    <div className="flex justify-center items-center gap-1">
+      //       <FiMenu
+      //          className="text-2xl mr-2"
+      //          onClick={() => setOpen(true)}
+      //       />
+      //       <img src={logo} alt="" className="h-[35px] rounded-full" />
+      //       <p className="text-2xl">Base</p>
+      //    </div>
+      //    <div className="flex gap-5">
+      //       <FaRegBell className="text-2xl" />
+      //       <img
+      //          src={person}
+      //          alt=""
+      //          className="h-[25px] p-[1px] rounded-full border-2 border-black"
+      //       />
+      //    </div>
+      //  </div>
+      //       <Main />
+      //    </div>
+      // </>
    )
 }
 
